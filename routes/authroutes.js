fa7967d60202feca8e9771e34c1badf403ec88a4
Pushkraj Vyavahare <163556662+alwaysgodly@users.noreply.github.com>
2024-09-28 21:23:49 +0530

    Add files via upload

---
+++ routes/authroutes.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+const router = express.Router();
+
+//  JWT token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+};
+
+// Register a new user
+router.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    user = new User({ name, email, password });
+    await user.save();
+
+    const token = generateToken(user._id);
+    res.status(201).json({ token });
+  } catch (error) {
+    res.status(500).json({ error: 'Server Error' });
+  }
+});
+
+// Login user
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    const token = generateToken(user._id);
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ error: 'Server Error' });
+  }
+});
+
+module.exports = router;
